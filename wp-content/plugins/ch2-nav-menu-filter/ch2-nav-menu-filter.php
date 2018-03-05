42ba1b79de7ef863213718ea236f2836f6779a90
Dan Crews <dancrews@me.com>
2018-03-05 16:00:01 -0500

    Hide menu item if not logged in.

---
+++ wp-content/plugins/ch2-nav-menu-filter/ch2-nav-menu-filter.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Chapter 2 - Nav Menu Filter
+Plugin URI:
+Description: Declares a plugin that will hide an item from the navigation menu for users who are not logged in.
+Version: 1.0
+Author: Dan Crews
+Author URI:
+LicenseL GPLv2
+*/
+
+add_filter( 'wp_nav_menu_objects', 'ch2nmf_new_nav_menu_items', 10, 2 );
+
+function ch2nmf_new_nav_menu_items( $sorted_menu_items, $args ) {
+	// Check if user is logged in, continue if not logged
+	if ( is_user_logged_in() == FALSE ) {
+		// Loop through all menu items received
+		// Place each item's key in $key variable
+		foreach ( $sorted_menu_items as $key => $sorted_menu_item ) {
+			// Check if menu item title matches search string
+			if ( 'Private Area' == $sorted_menu_item->title ) {
+				// Remove item from menu array if found using item key
+				unset( $sorted_menu_items[ $key ] );
+			}
+		}
+	}
+	return $sorted_menu_items;
+}
