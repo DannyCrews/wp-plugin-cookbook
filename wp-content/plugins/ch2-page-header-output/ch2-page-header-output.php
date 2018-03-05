6dc8cc701415e98287283a6a9a1ff87c4349eb30
Dan Crews <dancrews@me.com>
2018-03-05 11:31:18 -0500

    Add post visit tracking

--- wp-content/plugins/ch2-page-header-output/ch2-page-header-output.php
+++ wp-content/plugins/ch2-page-header-output/ch2-page-header-output.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Chapter 2 - Page Header Output
 Plugin URI:
-Description: Declares a plugin that will add content to the page header
+Description: Declares a plugin that will add content to the page header and footer
 Version: 1.0
 Author: Dan Crews
 Author URI:
@@ -17,7 +17,7 @@ function ch2pho_page_header_output() { ?>
 			i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
 			a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;.a.src=g;
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
 			m.parentNode.insertBefore(a,m)})(window,document,'script',
 			'https://www.google-analytics.com/analytics.js','ga');
 
@@ -25,3 +25,29 @@ function ch2pho_page_header_output() { ?>
 			ga('send', 'pageview');
 		</script>
 	<?php }
+
+add_filter( 'the_content', 'ch2lfa_link_filter_analytics' );
+
+function ch2lfa_link_filter_analytics ( $the_content ) {
+	$new_content = str_replace( 'href', 'onClick="recordOutboundLink( this );return false;" href', $the_content );
+
+	return $new_content;
+}
+
+add_action ( 'wp_footer', 'ch2lfa_footer_analytics_code' );
+
+function ch2lfa_footer_analytics_code() { ?>
+
+<script type="text/javascript">
+	function recordOutboundLink ( link ) {
+		ga( 'send', 'event', 'Outbound Links', 'Click',
+			link.href, {
+				'transport': 'beacon',
+				'hitCallback': function() {
+					document.location = link.href;
+				}
+			} );
+	}
+</script>
+
+<?php }
